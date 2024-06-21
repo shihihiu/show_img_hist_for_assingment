eead11b401174b3935ea399cb0c91ac716cac638
Shion Takeshima <cojcoj1616@gmail.com>
2024-06-21 19:27:46 +0900

    test1

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_image():
 
     st.image(
         im,
-        caption='image from wikimedia commons',
+        caption='image from wikimedia commons01',
         use_column_width=True
     )
 
